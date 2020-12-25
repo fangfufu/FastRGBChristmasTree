87aba9f9f9fc7260d25f288a736b109cd2cb85cb
Fufu Fang <fangfufu2003@gmail.com>
2020-12-25 13:20:27 +0000

    Added more functions to tree.py, added random spark example.

---
+++ sparkle.py
@@ -0,0 +1,20 @@
+from tree import FastRGBChristmasTree
+from colorzero import Color
+from random import random
+
+def random_colour():
+    h = random()
+    s = 1
+    v = 1
+    hsv = Color.from_hsv(h,s,v)
+    rgb = list(hsv.rgb)
+    rgb = [int(rgb[0]*255), int(rgb[1]*255), int(rgb[2]*255)]
+    return (rgb)
+
+if __name__ == '__main__':
+    tree = FastRGBChristmasTree()
+    while True:
+        for i in range(0,25):
+            rgb = random_colour()
+            tree[i] = [1, rgb[0], rgb[1], rgb[2]]
+        tree.commit()
--- tree.py
+++ tree.py
@@ -1,62 +1,98 @@
 from gpiozero import SPIDevice, SourceMixin
-from colorzero import Color, Hue
 
 class FastRGBChristmasTree(SourceMixin, SPIDevice):
-    def __init__(self, brightness=0.5, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(FastRGBChristmasTree, self).__init__(mosi_pin=12, clock_pin=25, *args, **kwargs)
         # Number of LEDs
         self.nled = 25
-        # Start of array offset
-        self.offset = 4
+        # Start of array __offset
+        self.__offset = 4
         # frame padding
-        frame_start = [0] * (self.offset)
+        frame_s = [0] * (self.__offset)
         frame_end = [0] * 5
         # transmit buffer
-        self.__buf = frame_start + [0] * self.nled * 4 +frame_end
+        self.__buf = frame_s + [0] * self.nled * 4 +frame_end
+        self.reset()
+        self.off()
 
     def __len__(self):
         return self.nled
 
-    #def __getitem__(self, index):
+    def __setitem__(self, i, val):
+        if i > self.nled - 1 or i < 0:
+            raise IndexError("LED i must be between 0 and 24!")
+        if len(val) < 3 or len(val) > 4:
+            raise IndexError("The length of the val array must be between 3 and 4")
 
-    def __setitem__(self, index, value):
-        if index > self.nled or index < 0:
-            raise IndexError("LED index must be between 0 and 24!")
-        if len(value) < 3 or len(value) > 4:
-            raise IndexError("The length of the value array must be between 3 and 4")
-
-        for i in value:
+        for j in val:
             if i >255:
-                raise ValueError("The value must be between 0-255!")
+                raise ValueError("The val must be between 0-255!")
 
-        start = self.offset + index * 4
-        if len(value) == 3:
-            start += 1
+        s = self.__offset + i * 4
+        if len(val) == 3:
+            s += 1
         else:
-            value[0] = self.__brightness_converter(value[0])
+            val[0] = self.__brightness_convert(val[0])
 
-        self.__buf[start:start+len(value)] = value
+        self.__buf[s:s+len(val)] = val
+
+    def __getitem__(self, i):
+        if i > self.nled - 1 or i < 0:
+            raise IndexError("LED i must be between 0 and 24!")
+        s = self.__offset + i * 4
+        val = self.__buf[s:s+4]
+        val[0] = self.__brightness_revert(val[0])
+        return val
 
     def __del__(self):
         super(FastRGBChristmasTree, self).close()
 
-    def __brightness_converter(self, brightness):
-        if brightness > 31 or brightness < 0:
-            raise ValueError("The brightness must be between 0 and 31")
+    def __brightness_convert(self, val):
+        if val > 31 or val < 1:
+            raise ValueError("The brightness must be between 1 and 31")
         # 0b1110000 == 224
-        return 0b11100000 | int(brightness)
+        return 0b11100000 | int(val)
+
+    def __brightness_revert(self, val):
+        return 0b00011111 & val
 
     def commit(self):
-        #print(self.__buf)
+        print(self.__buf)
         self._spi.transfer(self.__buf)
+
+    def off(self):
+        for i in range(0, self.nled):
+            self.__setitem__(i, [1, 0, 0, 0])
+        self.commit()
+
+    def reset(self):
+        for i in range(0, len(self.__buf)):
+            self.__buf[i] = 0
+        self.commit()
+
+    @property
+    def brightness(self):
+        val = 0
+        for i in range(0, self.nled):
+            s = self.__offset + i * 4
+            val += self.__buf[s]
+        return val / self.nled
+
+    @brightness.setter
+    def brightness(self, val):
+        for i in range(0, self.nled):
+            s = self.__offset + i * 4
+            self.__buf[s] = self.__brightness_convert(val)
+
 
 if __name__ == '__main__':
     tree = FastRGBChristmasTree()
-    while True:
-        for i in range(0,25):
-            tree[i] = [15, 255, 255, 255]
+    tree.brightness = 1
+    for i in range(0, tree.nled):
+        print(i)
+        tree[i] = [255, 255, 255]
         tree.commit()
-        for i in range(0,25):
-            tree[i] = [15, 0, 0, 0]
-        tree.commit()
+        input()
+        tree.off()
+    #tree.off()
 
