bd5d79424ad604d498ef484fea552367e3a9f85f
Fufu Fang <fangfufu@fangfufu.co.uk>
2020-12-26 11:35:19 +0000

    enabled the use of the slice operator in __setitem__

--- tree.py
+++ tree.py
@@ -6,6 +6,11 @@ class FastRGBChristmasTree(SourceMixin, SPIDevice):
         super(FastRGBChristmasTree, self).__init__(mosi_pin=12, clock_pin=25, *args, **kwargs)
         # Number of LEDs
         self.nled = 25
+        # LED configuration array
+        self.leds = array([[24, 19, 7, 0, 16, 15, 6, 12],
+                          [23, 20, 8, 1, 17, 14, 5, 11],
+                          [22, 21, 9, 2, 18, 13, 4, 10],
+                          [3,  3,  3, 3, 3,  3,  3, 3 ]])
         # Start of array __offset
         self.__offset = 4
         # frame padding
@@ -19,15 +24,24 @@ class FastRGBChristmasTree(SourceMixin, SPIDevice):
     def __len__(self):
         return self.nled
 
-    def __setitem__(self, i, val):
+    def __setitem__(self, ind, val):
+        # Enable the use of the slice operator
+        if isinstance(ind, slice):
+            r_start = ind.start if ind.start is not None else 0
+            r_stop = ind.stop if ind.stop is not None else self.nled
+            r_step = ind.step if ind.step is not None else 1
+            for i in range(r_start, r_stop, r_step):
+                self.__setitem__(i, val)
+            return
+
         if len(val) < 3 or len(val) > 4:
             raise IndexError("The length of the val array must be between 3 and 4")
 
-        for j in val:
-            if j >255:
+        for i in val:
+            if i >255:
                 raise ValueError("The val must be between 0-255!")
 
-        s = self.__offset + i * 4
+        s = self.__offset + ind * 4
         if len(val) == 3:
             s += 1
         else:
@@ -35,8 +49,8 @@ class FastRGBChristmasTree(SourceMixin, SPIDevice):
 
         self.__buf[s:s+len(val)] = val
 
-    def __getitem__(self, i):
-        s = self.__offset + i * 4
+    def __getitem__(self, ind):
+        s = self.__offset + ind * 4
         val = self.__buf[s:s+4]
         val[0] = self.__brightness_revert(val[0])
         return val
@@ -85,10 +99,13 @@ if __name__ == '__main__':
     tree = FastRGBChristmasTree()
     tree.brightness = 1
     while True:
-        for i in range(0, tree.nled):
-            print(i)
-            tree[i] = [255, 255, 255]
-            tree.commit()
-            tree.off()
+        tree[1:tree.nled:2] = [255, 255, 255]
+        tree.commit()
+        tree[:] = [0, 0, 0]
+        tree.commit()
+        tree[2:tree.nled:2] = [255, 255, 255]
+        tree.commit()
+        tree[:] = [0, 0, 0]
+        tree.commit()
     #tree.off()
 
