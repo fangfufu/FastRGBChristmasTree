13e8e8105b224cb7a88f3ea00af57b266fb0bb8e
Fufu Fang <fangfufu2003@gmail.com>
2021-01-01 23:34:43 +0000

    added spin.py

---
+++ spin.py
@@ -0,0 +1,31 @@
+'''
+Configure the LED into a spinning swirl
+'''
+from tree import FastRGBChristmasTree
+from time import sleep
+
+if __name__ == '__main__':
+    tree = FastRGBChristmasTree()
+    i = list(range(0,8))
+
+
+    tree[3] = [255,255,255]
+    while True:
+        tree[:,i[0]] = [255, 0, 0]
+        tree[:,i[1]] = [0, 255, 0]
+        tree[:,i[2]] = [0, 0, 255]
+        tree[:,i[3]] = [255, 255, 0]
+        tree[:,i[4]] = [255, 0, 255]
+        tree[:,i[5]] = [0, 255, 255]
+        tree[:,i[6]] = [255, 255, 255]
+        tree[:,i[7]] = [255, 255, 255]
+
+        tree.commit()
+
+        t = i[0]
+        for j in range(0,7):
+            i[j] = i[j+1]
+        i[7] = t
+
+        sleep(0.5)
+
