f97c14c30f5a01ed8ad7e8c52a7b209ef549c213
Fufu Fang <fangfufu2003@gmail.com>
2021-01-01 23:21:20 +0000

    fixed bug

--- random-sparkles.py
+++ random-sparkles.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
     tree = FastRGBChristmasTree()
     while True:
         for i in range(0,25):
-            rgb = random_sparkle()
             on = 1 if random() > 0.5 else 0
             tree[i] = [1, on, on, on]
         tree.commit()
